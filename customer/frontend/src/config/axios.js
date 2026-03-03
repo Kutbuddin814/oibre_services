73474b3ff9980086fb000401b4aa91102471e360
kutbuddin shaikh <kutbuddinshaikh814@gmail.com>
2026-03-03 12:28:29 +0530

    email error solced

--- customer/frontend/src/config/axios.js
+++ customer/frontend/src/config/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import API_BASE_URL from "./api";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: API_BASE_URL,//no wooreis
   timeout: 120000 // 2 minutes timeout for all requests
 });
 
